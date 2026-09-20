6daf5b6bbdb635adc08bd518a3013ea18d659664
Lulu <126499701+Qinjingxue@users.noreply.github.com>
2026-09-20 22:51:30 +0800

    refactor(rar5): keep MT pool with decoder lifetime

--- native/sevenzip_bridge/7z2603-src/CPP/7zip/Compress/Rar5Decoder.h
+++ native/sevenzip_bridge/7z2603-src/CPP/7zip/Compress/Rar5Decoder.h
@@ -19,7 +19,7 @@ namespace NRar5 {
 
 class CBitDecoder;
 #ifndef Z7_ST
-class CRar5MtContext;
+class CRar5ParallelBlockPool;
 #endif
 
 struct CFilter
@@ -117,7 +117,8 @@ Z7_CLASS_IMP_NOQIB_2(
   Byte *_inputBuf;
 #ifndef Z7_ST
   UInt32 _numThreads;
-  friend class CRar5MtContext;
+  UInt32 _mtPoolWorkers;
+  CRar5ParallelBlockPool *_mtPool;
 #endif
 
   NHuffman::CDecoder<kNumHufBits, kMainTableSize,  k_NumHufTableBits_Main>  m_MainDecoder;
