3fe6ab906afb123353b1c3ccdb0cdb3925c69089
Lulu <126499701+Qinjingxue@users.noreply.github.com>
2026-09-20 13:46:24 +0800

    Drop extraction signature probe parameters

--- native/sevenzip_bridge/include/sevenzip_bridge/bridge.hpp
+++ native/sevenzip_bridge/include/sevenzip_bridge/bridge.hpp
@@ -281,9 +281,7 @@ ExtractArchiveResult extract_archive_with_parts(
     bool native_volume_input = false,
     std::shared_ptr<AsyncFileWriter> shared_writer = nullptr,
     std::size_t job_buffer_budget = 0,
-    std::shared_ptr<std::atomic<bool>> cancel_token = nullptr,
-    const std::wstring& signature_path = L"",
-    unsigned long long signature_offset = 0
+    std::shared_ptr<std::atomic<bool>> cancel_token = nullptr
 );
 
 ExtractArchiveResult extract_archive_with_ranges(
