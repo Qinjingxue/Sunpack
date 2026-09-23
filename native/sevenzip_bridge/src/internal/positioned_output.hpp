c379b0fd77041ec78cbfa4e60986a292fcc2b062
Lulu <126499701+Qinjingxue@users.noreply.github.com>
2026-09-23 12:27:48 +0800

    feat: add positioned extraction callback capability

--- native/sevenzip_bridge/src/internal/positioned_output.hpp
+++ native/sevenzip_bridge/src/internal/positioned_output.hpp
@@ -17,6 +17,23 @@ public:
         UInt32 *processed_size) noexcept = 0;
 };
 
+class PositionedExtractCallback
+{
+public:
+    virtual ~PositionedExtractCallback() = default;
+
+    virtual bool positioned_extract_available() const noexcept = 0;
+
+    virtual HRESULT get_positioned_stream(
+        UInt32 index,
+        ISequentialOutStream **stream,
+        Int32 ask_mode) noexcept = 0;
+
+    virtual HRESULT set_positioned_operation_result(
+        UInt32 index,
+        Int32 operation_result) noexcept = 0;
+};
+
 inline PositionedOutStream *positioned_out_stream(ISequentialOutStream *stream) noexcept
 {
     return dynamic_cast<PositionedOutStream *>(stream);
