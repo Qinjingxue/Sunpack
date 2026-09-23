99b007abe9ec6d3e186d24637aee6de858cc1e24
Lulu <126499701+Qinjingxue@users.noreply.github.com>
2026-09-23 13:56:11 +0800

    refactor: expose output capabilities through COM queries

--- native/sevenzip_bridge/src/internal/sevenzip_callbacks.hpp
+++ native/sevenzip_bridge/src/internal/sevenzip_callbacks.hpp
@@ -239,7 +239,7 @@ namespace sunpack::sevenzip
 
     class ExtractCallback final : public CMyUnknownImp, public IArchiveExtractCallback, public ICryptoGetTextPassword
     {
-        Z7_COM_UNKNOWN_IMP_3(IArchiveExtractCallback, IProgress, ICryptoGetTextPassword)
+        Z7_COM_UNKNOWN_IMP_4(IArchiveExtractCallback, IProgress, ICryptoGetTextPassword, ISunpackPositionedExtractCallback)
         
 
     public:
@@ -302,7 +302,7 @@ namespace sunpack::sevenzip
 
     class SynchronousFileOutStream final : public CMyUnknownImp, public ISequentialOutStream
     {
-        Z7_COM_UNKNOWN_IMP_1(ISequentialOutStream)
+        Z7_COM_UNKNOWN_IMP_2(ISequentialOutStream, ISunpackPositionedOutStream)
         
 
     public:
@@ -452,7 +452,7 @@ namespace sunpack::sevenzip
         UInt64 bytes_written_ = 0;
     };
 
-    class AsyncFileOutStream final : public CMyUnknownImp, public ISequentialOutStream, public PositionedOutStream
+    class AsyncFileOutStream final : public CMyUnknownImp, public ISequentialOutStream, public ISunpackPositionedOutStream
     {
         Z7_COM_UNKNOWN_IMP_1(ISequentialOutStream)
         
@@ -703,7 +703,7 @@ namespace sunpack::sevenzip
         return normalized;
     }
 
-    class ExtractToDiskCallback final : public CMyUnknownImp, public IArchiveExtractCallback, public ICryptoGetTextPassword, public PositionedExtractCallback
+    class ExtractToDiskCallback final : public CMyUnknownImp, public IArchiveExtractCallback, public ICryptoGetTextPassword, public ISunpackPositionedExtractCallback
     {
         Z7_COM_UNKNOWN_IMP_3(IArchiveExtractCallback, IProgress, ICryptoGetTextPassword)
         
