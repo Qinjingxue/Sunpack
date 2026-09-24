55c76d64c68d76722aba793f9571c87a1af29229
Lulu <126499701+Qinjingxue@users.noreply.github.com>
2026-09-24 23:56:35 +0800

    refactor: export encrypted embedded boundary resolver

--- sunpack/core/analysis/embedded/__init__.py
+++ sunpack/core/analysis/embedded/__init__.py
@@ -1,6 +1,10 @@
 from sunpack.core.analysis.embedded.carrier import inspect_runtime_bundle
 from sunpack.core.analysis.embedded.result import EmbeddedCandidate, EmbeddedScanResult, SignatureHit
-from sunpack.core.analysis.embedded.scanner import embedded_result_from_dict, scan_embedded_archives
+from sunpack.core.analysis.embedded.scanner import (
+    embedded_result_from_dict,
+    resolve_encrypted_rar_boundaries,
+    scan_embedded_archives,
+)
 
 __all__ = [
     "EmbeddedCandidate",
@@ -8,5 +12,6 @@ __all__ = [
     "SignatureHit",
     "embedded_result_from_dict",
     "inspect_runtime_bundle",
+    "resolve_encrypted_rar_boundaries",
     "scan_embedded_archives",
 ]
