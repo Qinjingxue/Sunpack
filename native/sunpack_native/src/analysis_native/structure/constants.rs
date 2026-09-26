96a099ccb784a9e29ec4ef9bbe6614f73778ff8c
Lulu <126499701+Qinjingxue@users.noreply.github.com>
2026-09-26 12:52:46 +0800

    refactor: remove obsolete ZIP local-header constant

--- native/sunpack_native/src/analysis_native/structure/constants.rs
+++ native/sunpack_native/src/analysis_native/structure/constants.rs
@@ -6,7 +6,6 @@ const ZIP64_EOCD_LOCATOR_SIGNATURE: &[u8] = b"PK\x06\x07";
 const ZIP_EOCD_MIN_SIZE: usize = 22;
 const ZIP_EOCD_MAX_COMMENT: u64 = 65_535;
 const ZIP_CENTRAL_DIRECTORY_HEADER_SIZE: usize = 46;
-const ZIP_LOCAL_HEADER_LENGTH: usize = 30;
 const ZIP64_EOCD_RECORD_SIZE: usize = 56;
 const ZIP64_EOCD_LOCATOR_SIZE: usize = 20;
 const TAR_BLOCK_SIZE: usize = 512;
